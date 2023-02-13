eda2f7ca6661b4431b6a52dadc0bc5c8b03d4d4c
Kwasi Adu <kwasi.adu@gmail.com>
2023-02-12 22:43:10 -0800

    build: add prettier to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App (): JSX.Element {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
@@ -50,7 +50,7 @@ function App (): JSX.Element {
         </span>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,18 @@
-import { configureStore, type ThunkAction, type Action } from '@reduxjs/toolkit'
+import {
+  configureStore,
+  type ThunkAction,
+  type Action,
+} from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-  }
-})
+  reducer: {},
+});
 
-export type AppDispatch = typeof store.dispatch
-export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-ReturnType,
-RootState,
-unknown,
-Action<string>
->
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
