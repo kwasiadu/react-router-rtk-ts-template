3881d04fb4b0b2baf89f768e3a656fb1c3ce628e
Kwasi Adu <kwasi.adu@gmail.com>
2023-02-13 00:41:11 -0800

    build: add storybook addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,14 @@ module.exports = {
   stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
   /* Tell storybook where the static folder is */
   staticDirs: ["../public"],
-  // addons: [
-  //   "@storybook/addon-links",
-  //   "@storybook/addon-essentials",
-  //   "@storybook/addon-interactions",
-  // ],
-  // framework: "@storybook/react",
-  // core: {
-  //   builder: "@storybook/builder-webpack5",
-  // },
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
 };
